26ce4ac1eeafde199ce7f488363eb343326a1da1
sgpublic <sgpublic2002@gmail.com>
2024-07-19 22:23:10 +0800

    feat: new login check

--- app/plugins/modules/siteconf_patch.py
+++ app/plugins/modules/siteconf_patch.py
@@ -4,8 +4,16 @@ from app.helper import SiteHelper
 from lxml import etree
 
 log.info(f"【Plugin】加载 SiteConf Patch！")
+
 SiteConf()._SITE_CHECKIN_XPATH.append('//img[@id="fx_checkin_b"]')
+
+SiteConf()._SITE_LOGIN_XPATH["captcha"].insert(0, '//input[@name="seccodeverify"]')
+
+SiteConf()._SITE_LOGIN_XPATH["captcha_img"].insert(0, '//span[contains(@id, "vseccode_")]//img')
+
 SiteConf()._SITE_LOGIN_XPATH["submit"].insert(0, '//button[@name="loginsubmit"]')
+
+SiteConf()._SITE_LOGIN_XPATH["error"].insert(0, '//div[@id="ntcwin"]//div[contains(@class, "pc_inner")]')
 
 _SiteHelper_is_logged_in = SiteHelper.is_logged_in
 
@@ -16,8 +24,9 @@ def SiteHelper_is_logged_in(html_text=None):
     html = etree.HTML(html_text)
     # 是否存在登出和用户面板等链接
     xpaths = [
-        '//div[@id, "main_succeed"]',
-        '//a[@id, "myitem"]',
+        '//p[@id="succeedmessage"]',
+        '//p[@id="succeedlocation"]',
+        '//a[@id="myitem"]',
     ]
     for xpath in xpaths:
         if html.xpath(xpath):
